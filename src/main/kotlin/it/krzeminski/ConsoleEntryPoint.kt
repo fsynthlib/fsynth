ef520ec0b650dd30965f61e26e26b470ad5af9a0
Piotr Krzeminski <git@krzeminski.it>
2018-06-03 22:05:07 +0200

    Initial commit: hello world CLI

---
+++ src/main/kotlin/it/krzeminski/ConsoleEntryPoint.kt
@@ -0,0 +1,5 @@
+package it.krzeminski
+
+fun main(args: Array<String>) =
+    println("Hello, World")
+
---
+++ src/test/kotlin/it/krzeminski/HelloTest.kt
@@ -0,0 +1,8 @@
+package it.krzeminski
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
