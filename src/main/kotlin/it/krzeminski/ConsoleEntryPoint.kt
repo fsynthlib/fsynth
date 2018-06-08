2d49d751974e10f5f03617e9dc97df9e05cfe822
Piotr Krzeminski <git@krzeminski.it>
2018-06-08 18:46:52 +0200

    Play more complicated song
    ...to make further experimenting easier.

--- src/main/kotlin/it/krzeminski/ConsoleEntryPoint.kt
+++ src/main/kotlin/it/krzeminski/ConsoleEntryPoint.kt
@@ -8,23 +8,29 @@ fun normalizedWaveValueToCharacter(value: Float): Char =
 
 fun noteIndexToFrequency(index: Int) = pow(2.0, (index - 9).toDouble()/12.0).toFloat()*440.0f/32.0f
 
-fun playNote(index: Int) {
+fun playNote(index: Int, samples: Int) {
     val sineWaveForFrequency = sineWave(noteIndexToFrequency(index))
-    for (t in 0..800) {
+    for (t in 0..samples) {
         print(normalizedWaveValueToCharacter(sineWaveForFrequency(t.toFloat()/8000.0f)))
+    }
+}
+
+fun silence(samples: Int) {
+    for (t in 0..samples) {
+        print(normalizedWaveValueToCharacter(0.0f))
     }
 }
 
 fun main(args: Array<String>) {
     System.setOut(PrintStream(System.out, true, "ISO8859_1"))
 
-    playNote(60)
-    playNote(62)
-    playNote(64)
-    playNote(65)
-    playNote(67)
-    playNote(69)
-    playNote(71)
-    playNote(72)
+    playNote(62, 2000)
+    playNote(61, 1000)
+    playNote(62, 1000)
+    playNote(64, 2000)
+    playNote(62, 2000)
+    silence(2000)
+    playNote(66, 2000)
+    playNote(67, 4000)
 }
 
