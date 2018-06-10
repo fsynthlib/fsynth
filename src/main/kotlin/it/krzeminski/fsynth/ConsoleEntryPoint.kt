3c5445528153cae10cfcbefb9922c2550731fe81
Piotr Krzeminski <git@krzeminski.it>
2018-06-10 11:38:29 +0200

    Extract demo song to a separate module

--- src/main/kotlin/it/krzeminski/fsynth/ConsoleEntryPoint.kt
+++ src/main/kotlin/it/krzeminski/fsynth/ConsoleEntryPoint.kt
@@ -1,26 +1,11 @@
 package it.krzeminski.fsynth
 
-import it.krzeminski.fsynth.MusicNote.*
+import it.krzeminski.fsynth.songs.simpleDemoSong
 
 fun sineWaveForNote(note: MusicNote) = sineWave(note.frequency)
-
-val silence = { _: Float -> 0.0f }
-
-val demoSong: (Float) -> Float = { time ->
-    when (time) {
-        in 0.0f..0.25f -> sineWaveForNote(D4)
-        in 0.25f..0.375f -> sineWaveForNote(Csharp4)
-        in 0.375f..0.5f -> sineWaveForNote(D4)
-        in 0.5f..0.75f -> sineWaveForNote(E4)
-        in 0.75f..1.0f -> sineWaveForNote(D4)
-        in 1.25f..1.5f -> sineWaveForNote(Fsharp4)
-        in 1.5f..2.0f -> sineWaveForNote(G4)
-        else -> silence
-    }(time)
-}
 
 fun main(args: Array<String>) {
     configureOutputFormat()
 
-    render8bit(wave = demoSong, length = 2.0f, sampleRate = 8000)
+    render8bit(wave = simpleDemoSong, length = 2.0f, sampleRate = 8000)
 }
---
+++ src/main/kotlin/it/krzeminski/fsynth/songs/SimpleDemoSong.kt
@@ -0,0 +1,19 @@
+package it.krzeminski.fsynth.songs
+
+import it.krzeminski.fsynth.MusicNote.*
+import it.krzeminski.fsynth.sineWaveForNote
+
+val silence = { _: Float -> 0.0f }
+
+val simpleDemoSong: (Float) -> Float = { time ->
+    when (time) {
+        in 0.0f..0.25f -> sineWaveForNote(D4)
+        in 0.25f..0.375f -> sineWaveForNote(Csharp4)
+        in 0.375f..0.5f -> sineWaveForNote(D4)
+        in 0.5f..0.75f -> sineWaveForNote(E4)
+        in 0.75f..1.0f -> sineWaveForNote(D4)
+        in 1.25f..1.5f -> sineWaveForNote(Fsharp4)
+        in 1.5f..2.0f -> sineWaveForNote(G4)
+        else -> silence
+    }(time)
+}
