86ad25935fffcd2ffa55edb5714cddc3ee0356a2
Piotr Krzeminski <git@krzeminski.it>
2018-06-25 23:00:15 +0200

    Create a simple DSL for describing songs

--- core/src/it/krzeminski/fsynth/songs/SimpleDemoSong.kt
+++ core/src/it/krzeminski/fsynth/songs/SimpleDemoSong.kt
@@ -1,28 +1,18 @@
 package it.krzeminski.fsynth.songs
 
 import it.krzeminski.fsynth.instruments.organs
-import it.krzeminski.fsynth.silence
 import it.krzeminski.fsynth.types.MusicNote.*
-import it.krzeminski.fsynth.types.Song
-import it.krzeminski.fsynth.types.Waveform
-import it.krzeminski.fsynth.types.plus
-import it.krzeminski.fsynth.types.times
+import it.krzeminski.fsynth.types.song
 
-private val melody: Waveform = { time ->
-    when (time) {
-        in 0.0f..0.25f -> organs(D4)
-        in 0.25f..0.375f -> organs(Csharp4)
-        in 0.375f..0.5f -> organs(D4)
-        in 0.5f..0.75f -> organs(E4)
-        in 0.75f..1.0f -> organs(D4)
-        in 1.25f..1.5f -> organs(A3) + organs(D4) + organs(Fsharp4)
-        in 1.5f..2.0f -> organs(B3) + organs(D4) + organs(G4)
-        else -> silence
-    }(time)
+// The "volume" parameter is needed to avoid generating values from outside <-1.0; 1.0>.
+// TODO: Handle this issue properly, maybe compute maximum volume automatically and scale appropriately.
+val simpleDemoSong = song(instrument=::organs, volume=0.3f) {
+    note(0.25f, D4)
+    note(0.125f, Csharp4)
+    note(0.125f, D4)
+    note(0.25f, E4)
+    note(0.25f, D4)
+    pause(0.25f)
+    chord(0.25f, A3, D4, Fsharp4)
+    chord(0.5f, B3, D4, G4)
 }
-
-// Needed to avoid generating values from outside <-1.0; 1.0>, effectively lowering the volume.
-// TODO: Handle this issue properly.
-private val simpleDemoSongWaveform = 0.3*melody
-
-val simpleDemoSong = Song(waveform = simpleDemoSongWaveform, durationInSeconds = 2.0f)
--- core/src/it/krzeminski/fsynth/types/Song.kt
+++ core/src/it/krzeminski/fsynth/types/Song.kt
@@ -1,3 +1,51 @@
 package it.krzeminski.fsynth.types
 
+import it.krzeminski.fsynth.silence
+
 data class Song(val waveform: Waveform, val durationInSeconds: Float)
+
+fun song(instrument: (MusicNote) -> Waveform, volume: Float, init: SongBuilder.() -> Unit): Song {
+    val songBuilder = SongBuilder(instrument, volume)
+    songBuilder.init()
+    return songBuilder.build()
+}
+
+private data class SongSegment(val waveform: Waveform, val durationInSeconds: Float)
+
+class SongBuilder(private val instrument: (MusicNote) -> Waveform, private val volume: Float) {
+    private val segments = mutableListOf<SongSegment>()
+
+    fun note(durationInSeconds: Float, note: MusicNote) {
+        segments += SongSegment(instrument(note), durationInSeconds)
+    }
+
+    fun chord(durationInSeconds: Float, vararg notes: MusicNote) {
+        segments += SongSegment(
+                notes.asSequence()
+                        .map(instrument)
+                        .fold(silence) { accumulator, current -> accumulator + current },
+                durationInSeconds)
+    }
+
+    fun pause(durationInSeconds: Float) {
+        segments += SongSegment(silence, durationInSeconds)
+    }
+
+    fun build(): Song {
+        val durationInSeconds = segments.map { it.durationInSeconds }.sum()
+        return Song(
+                waveform = ::getSongWaveformValue,
+                durationInSeconds = durationInSeconds)
+    }
+
+    private fun getSongWaveformValue(time: Float): Float {
+        var cumulatedDuration = 0.0f
+        for (segment in segments) {
+            if (time in cumulatedDuration..(cumulatedDuration + segment.durationInSeconds)) {
+                return segment.waveform(time)*volume
+            }
+            cumulatedDuration += segment.durationInSeconds
+        }
+        return 0.0f
+    }
+}
