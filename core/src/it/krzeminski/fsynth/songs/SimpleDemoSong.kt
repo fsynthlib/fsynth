17c7cddd316768c98edda722aa867c1f7ae2c173
Piotr Krzeminski <git@krzeminski.it>
2018-06-26 14:24:17 +0200

    Specify note value using DSL

--- core/src/it/krzeminski/fsynth/songs/SimpleDemoSong.kt
+++ core/src/it/krzeminski/fsynth/songs/SimpleDemoSong.kt
@@ -2,17 +2,23 @@ package it.krzeminski.fsynth.songs
 
 import it.krzeminski.fsynth.instruments.organs
 import it.krzeminski.fsynth.types.MusicNote.*
+import it.krzeminski.fsynth.types.by
 import it.krzeminski.fsynth.types.song
 
 // The "volume" parameter is needed to avoid generating values from outside <-1.0; 1.0>.
 // TODO: Handle this issue properly, maybe compute maximum volume automatically and scale appropriately.
-val simpleDemoSong = song(instrument=::organs, volume=0.3f) {
-    note(0.25f, D4)
-    note(0.125f, Csharp4)
-    note(0.125f, D4)
-    note(0.25f, E4)
-    note(0.25f, D4)
-    pause(0.25f)
-    chord(0.25f, A3, D4, Fsharp4)
-    chord(0.5f, B3, D4, G4)
+val simpleDemoSong = song(
+        beatsPerMinute = 120,
+        instrument=::organs,
+        volume=0.3f)
+{
+    note(1 by 8, D4)
+    note(1 by 16, Csharp4)
+    note(1 by 16, D4)
+    note(1 by 8, E4)
+    note(1 by 8, D4)
+
+    pause(1 by 8)
+    chord(1 by 8, A3, D4, Fsharp4)
+    chord(1 by 4, B3, D4, G4)
 }
--- core/src/it/krzeminski/fsynth/types/Song.kt
+++ core/src/it/krzeminski/fsynth/types/Song.kt
@@ -4,31 +4,35 @@ import it.krzeminski.fsynth.silence
 
 data class Song(val waveform: Waveform, val durationInSeconds: Float)
 
-fun song(instrument: (MusicNote) -> Waveform, volume: Float, init: SongBuilder.() -> Unit): Song {
-    val songBuilder = SongBuilder(instrument, volume)
+fun song(beatsPerMinute: Int, instrument: (MusicNote) -> Waveform, volume: Float, init: SongBuilder.() -> Unit): Song {
+    val songBuilder = SongBuilder(beatsPerMinute, instrument, volume)
     songBuilder.init()
     return songBuilder.build()
 }
 
 private data class SongSegment(val waveform: Waveform, val durationInSeconds: Float)
 
-class SongBuilder(private val instrument: (MusicNote) -> Waveform, private val volume: Float) {
+class SongBuilder(
+        private val beatsPerMinute: Int,
+        private val instrument: (MusicNote) -> Waveform,
+        private val volume: Float)
+{
     private val segments = mutableListOf<SongSegment>()
 
-    fun note(durationInSeconds: Float, note: MusicNote) {
-        segments += SongSegment(instrument(note), durationInSeconds)
+    fun note(noteValue: Float, note: MusicNote) {
+        segments += SongSegment(instrument(note), noteValue.toSeconds())
     }
 
-    fun chord(durationInSeconds: Float, vararg notes: MusicNote) {
+    fun chord(noteValue: Float, vararg notes: MusicNote) {
         segments += SongSegment(
                 notes.asSequence()
                         .map(instrument)
                         .fold(silence) { accumulator, current -> accumulator + current },
-                durationInSeconds)
+                noteValue.toSeconds())
     }
 
-    fun pause(durationInSeconds: Float) {
-        segments += SongSegment(silence, durationInSeconds)
+    fun pause(noteValue: Float) {
+        segments += SongSegment(silence, noteValue.toSeconds())
     }
 
     fun build(): Song {
@@ -48,4 +52,13 @@ class SongBuilder(private val instrument: (MusicNote) -> Waveform, private val v
         }
         return 0.0f
     }
+
+    private fun Float.toSeconds() = this*BEATS_PER_BAR*SECONDS_IN_MINUTE/beatsPerMinute.toFloat()
 }
+
+// Constant for now.
+// TODO: generalize it (may be not always 4).
+private const val BEATS_PER_BAR = 4
+private const val SECONDS_IN_MINUTE = 60
+
+infix fun Int.by(denominator: Int): Float = this.toFloat()/denominator.toFloat()
