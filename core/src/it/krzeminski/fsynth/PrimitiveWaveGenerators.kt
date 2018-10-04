72efa895c8e9df4b7b395094ed422c2f46f50a55
Piotr Krzeminski <git@krzeminski.it>
2018-10-04 16:01:35 +0200

    Fix square wave to be 1 at t=0
    The difference in the generated sound is not heard, it's just
    to make the tests (that are to come in the next commit) look
    more straightforward.

--- core/src/it/krzeminski/fsynth/PrimitiveWaveGenerators.kt
+++ core/src/it/krzeminski/fsynth/PrimitiveWaveGenerators.kt
@@ -8,4 +8,4 @@ val silence = { _: Float -> 0.0f }
 
 fun sineWave(frequency: Float): Waveform = { t -> sin(frequency*t*2.0*PI).toFloat() }
 
-fun squareWave(frequency: Float): Waveform = { t -> if (sineWave(frequency)(t) > 0.0f) 1.0f else -1.0f }
+fun squareWave(frequency: Float): Waveform = { t -> if (sineWave(frequency)(t) >= 0.0f) 1.0f else -1.0f }
