7bcdb148b587b894b0375a74b276b0ac180e9d2b
Piotr Krzeminski <git@krzeminski.it>
2018-10-09 22:29:29 +0200

    Web version: add a note about long synthesis

--- web/src/main/kotlin/it/krzeminski/fsynth/WebDemo.kt
+++ web/src/main/kotlin/it/krzeminski/fsynth/WebDemo.kt
@@ -2,6 +2,7 @@ package it.krzeminski.fsynth
 
 import it.krzeminski.fsynth.songs.allSongs
 import it.krzeminski.fsynth.typings.materialButton
+import react.dom.a
 import react.dom.br
 import react.dom.div
 import react.dom.render
@@ -11,6 +12,11 @@ fun main(args: Array<String>) {
     render(document.getElementById("root")) {
         div {
             +"The sound is synthesized and played after clicking the chosen button below."
+        }
+        div {
+            +"The synthesis may take some time, even up to 1 minute and block the UI,"
+            +" so be patient. Optimization in scope of "
+            a("https://github.com/krzema12/fsynth/issues/12") { +"issue #12" }
         }
         allSongs.forEach { song ->
             materialButton {
