ad52aedc48c6c6839919200ef907e47c0ff06a0d
Piotr Krzeminski <git@krzeminski.it>
2018-10-18 14:07:28 +0200

    Add performance tests, ran manually
    Part of #12.

---
+++ cli/src/test/kotlin/performance/RenderingTimeAndNumberOfSegments.kt
@@ -0,0 +1,24 @@
+package performance
+
+import it.krzeminski.fsynth.instruments.synthesizer
+import it.krzeminski.fsynth.renderWithSampleRate
+import it.krzeminski.fsynth.types.MusicNote
+import it.krzeminski.fsynth.types.by
+import it.krzeminski.fsynth.types.song
+import kotlin.system.measureTimeMillis
+
+fun main(args: Array<String>) {
+    (1..1000).forEach { numberOfSegments ->
+        val testSong = song(name = "Test song", beatsPerMinute = 100, volume = 1.0f) {
+            track(instrument = ::synthesizer) {
+                repeat(numberOfSegments) {
+                    note(1 by 4, MusicNote.A4)
+                }
+            }
+        }
+        val timeMillis = measureTimeMillis {
+            testSong.renderWithSampleRate(44100).forEach {  }
+        }
+        println("Segments: $numberOfSegments, time in ms: $timeMillis")
+    }
+}
