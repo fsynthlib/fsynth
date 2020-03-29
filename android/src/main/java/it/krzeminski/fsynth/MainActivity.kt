1c0af0e05f93e6679db0e8224fdcfdc245d04fd4
Piotr Krzeminski <git@krzeminski.it>
2020-03-29 22:11:31 +0200

    Android app: display buttons for available songs
    Part of #24.

--- android/src/main/java/it/krzeminski/fsynth/MainActivity.kt
+++ android/src/main/java/it/krzeminski/fsynth/MainActivity.kt
@@ -2,10 +2,24 @@ package it.krzeminski.fsynth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.core.view.plusAssign
+import it.krzeminski.fsynth.songs.allSongs
+import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        allSongs.forEach { song ->
+            buttons += Button(this).apply {
+                text = song.name
+                setOnClickListener {
+                    println("TODO: synthesize ${song.name}")
+                }
+            }
+        }
     }
 }
