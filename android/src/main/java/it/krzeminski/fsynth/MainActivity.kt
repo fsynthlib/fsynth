618d458b28dfb6c05ef753fc79623959c704d802
Piotr Krzeminski <git@krzeminski.it>
2020-03-30 21:47:49 +0200

    Android app: synthesize and play songs in blocking manner
    The first, simplest implementation. Non-blocking UI and Material UI
    will follow.
    
    Part of #24.

--- android/src/main/java/it/krzeminski/fsynth/MainActivity.kt
+++ android/src/main/java/it/krzeminski/fsynth/MainActivity.kt
@@ -1,10 +1,15 @@
 package it.krzeminski.fsynth
 
+import android.media.AudioFormat
+import android.media.AudioManager
+import android.media.AudioTrack
+import android.media.AudioTrack.WRITE_BLOCKING
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import androidx.core.view.plusAssign
 import it.krzeminski.fsynth.songs.allSongs
+import it.krzeminski.fsynth.types.Song
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,9 +22,24 @@ class MainActivity : AppCompatActivity() {
             buttons += Button(this).apply {
                 text = song.name
                 setOnClickListener {
-                    println("TODO: synthesize ${song.name}")
+                    song.playOnAndroid(sampleRate = 44100)
                 }
             }
         }
+    }
+}
+
+private fun Song.playOnAndroid(sampleRate: Int) {
+    val samples = renderWithSampleRate(sampleRate, 0.0f).toList()
+    val audioTrack = AudioTrack(
+            AudioManager.STREAM_MUSIC,
+            sampleRate,
+            AudioFormat.CHANNEL_OUT_MONO,
+            AudioFormat.ENCODING_PCM_FLOAT,
+            samples.size*java.lang.Float.BYTES,
+            AudioTrack.MODE_STATIC)
+    with (audioTrack) {
+        write(samples.toFloatArray(), 0, samples.size, WRITE_BLOCKING)
+        play()
     }
 }
