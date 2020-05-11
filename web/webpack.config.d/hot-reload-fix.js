8b7ea3574bc492c84b4d3f7c5727504ec3e65a26
Piotr Krzeminski <git@krzeminski.it>
2020-05-11 19:11:16 +0200

    Fix hot-reloading of web page during development
    There's a bug in Kotlin that makes hot-reloading not work properly:
    ttps://youtrack.jetbrains.com/issue/KT-32273. The Webpack plugin being added
    was found in this bug's comments. It seems to work.
    
    One note: the file has to be actually saved to trigger the reload, so e.g.
    window focus has to go away from IntelliJ.
    
    Resolves #56.

---
+++ web/webpack.config.d/hot-reload-fix.js
@@ -0,0 +1,37 @@
+// XXX: This Webpack plugin is a workaround for hot reload not working out-of-the-box in Kotlin JS plugin.
+// See https://youtrack.jetbrains.com/issue/KT-32273
+
+if (config.devServer) {
+    config.devServer.watchOptions = {
+        aggregateTimeout: 300,
+        poll: 300
+    };
+    config.devServer.stats = {
+        warnings: false
+    };
+    config.devServer.clientLogLevel = 'error';
+}
+
+class KvWebpackPlugin {
+    apply(compiler) {
+        const fs = require('fs')
+        compiler.hooks.watchRun.tapAsync("KvWebpackPlugin", (compiler, callback) => {
+            var runCallback = true;
+            for (let item of compiler.removedFiles.values()) {
+                if (item == config.entry.main) {
+                    if (!fs.existsSync(item)) {
+                        fs.watchFile(item, {interval: 50}, (current, previous) => {
+                            if (current.ino > 0) {
+                                fs.unwatchFile(item);
+                                callback();
+                            }
+                        });
+                        runCallback = false;
+                    }
+                }
+            }
+            if (runCallback) callback();
+        });
+    }
+};
+config.plugins.push(new KvWebpackPlugin())
