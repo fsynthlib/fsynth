3dafddda10a558c4559b3dc3830b7d3767987da6
Piotr Krzeminski <git@krzeminski.it>
2020-05-12 17:30:37 +0200

    Remove workaround for running JS tests
    Apparently with the migration to the new Kotlin JS plugin,
    the problem disappeared.

--- web/src/main/kotlin/it/krzeminski/fsynth/main.kt
+++ web/src/main/kotlin/it/krzeminski/fsynth/main.kt
@@ -5,18 +5,11 @@ import react.dom.render
 import kotlin.browser.document
 
 fun main(args: Array<String>) {
-    try {
-        render(document.getElementById("root")) {
-            player {
-                attrs {
-                    songs = allSongs
-                }
+    render(document.getElementById("root")) {
+        player {
+            attrs {
+                songs = allSongs
             }
         }
-    } catch (e: dynamic) {
-        // This try-catch is a workaround needed to run the unit tests successfully.
-        // It's not known why the test runner treats this entry point as another unit test. It cannot find the element
-        // with ID "root", and it fails, throwing an exception that is caught in this 'catch'.
-        // In production, the element is there, defined in index.html, and the exception isn't thrown.
     }
 }
