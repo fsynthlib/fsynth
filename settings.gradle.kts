35a5c8050a3a3c006ac450d7c72d01d9cf90363b
Piotr Krzeminski <git@krzeminski.it>
2026-05-20 23:41:56 +0200

    Fix looking for org.ajoberstar.grgit:grgit-core:4.1.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,11 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+}
+
 rootProject.name = "fsynth"
 
 include("core", "cli", "web", ":web:worker", ":web:serviceworker", "android")
