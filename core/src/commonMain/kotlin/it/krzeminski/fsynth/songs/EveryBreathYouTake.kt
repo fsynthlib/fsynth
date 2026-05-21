3e2b3ae8655027ada741a010f67cfbcec605cef3
Piotr Krzeminski <git@krzeminski.it>
2026-05-21 10:06:09 +0200

    Fix vocal melody alignment: correct note values from MIDI, add 8-bar intro

--- core/src/commonMain/kotlin/it/krzeminski/fsynth/songs/EveryBreathYouTake.kt
+++ core/src/commonMain/kotlin/it/krzeminski/fsynth/songs/EveryBreathYouTake.kt
@@ -23,92 +23,71 @@ val everyBreathYouTake = song(
             note(1 by 8, third)
         }
 
-        arpeggio(A3, Csharp4, E4, A4)
-        arpeggio(A3, Csharp4, E4, A4)
-        arpeggio(Fsharp3, A3, Csharp4, Fsharp4)
-        arpeggio(Fsharp3, A3, Csharp4, Fsharp4)
-        arpeggio(D3, Fsharp3, A3, D4)
-        arpeggio(E3, Gsharp3, B3, E4)
-        arpeggio(A3, Csharp4, E4, A4)
-        arpeggio(A3, Csharp4, E4, A4)
+        repeat(2) {
+            arpeggio(A3, Csharp4, E4, A4)
+            arpeggio(A3, Csharp4, E4, A4)
+            arpeggio(Fsharp3, A3, Csharp4, Fsharp4)
+            arpeggio(Fsharp3, A3, Csharp4, Fsharp4)
+            arpeggio(D3, Fsharp3, A3, D4)
+            arpeggio(E3, Gsharp3, B3, E4)
+            arpeggio(A3, Csharp4, E4, A4)
+            arpeggio(A3, Csharp4, E4, A4)
+        }
     }
 
     track(name = "Bass", instrument = simpleDecayEnvelopeSynthesizer, volume = 0.2f) {
-        repeat(8) { note(1 by 8, A1) }
-        note(1 by 8, A1)
-        note(1 by 8, A1)
-        note(1 by 8, A1)
-        note(1 by 8, A1)
-        note(1 by 8, A1)
-        note(1 by 8, A1)
-        note(1 by 8, A1)
-        note(1 by 8, Csharp2)
-        repeat(8) { note(1 by 8, Fsharp1) }
-        repeat(8) { note(1 by 8, Fsharp1) }
-        repeat(8) { note(1 by 8, D2) }
-        repeat(8) { note(1 by 8, E2) }
-        repeat(8) { note(1 by 8, A1) }
-        note(1 by 8, A1)
-        note(1 by 8, A1)
-        note(1 by 8, A1)
-        note(1 by 8, A1)
-        note(1 by 8, A1)
-        note(1 by 8, A1)
-        note(1 by 8, A1)
-        note(1 by 8, Csharp2)
+        repeat(2) {
+            repeat(8) { note(1 by 8, A1) }
+            note(1 by 8, A1)
+            note(1 by 8, A1)
+            note(1 by 8, A1)
+            note(1 by 8, A1)
+            note(1 by 8, A1)
+            note(1 by 8, A1)
+            note(1 by 8, A1)
+            note(1 by 8, Csharp2)
+            repeat(8) { note(1 by 8, Fsharp1) }
+            repeat(8) { note(1 by 8, Fsharp1) }
+            repeat(8) { note(1 by 8, D2) }
+            repeat(8) { note(1 by 8, E2) }
+            repeat(8) { note(1 by 8, A1) }
+            note(1 by 8, A1)
+            note(1 by 8, A1)
+            note(1 by 8, A1)
+            note(1 by 8, A1)
+            note(1 by 8, A1)
+            note(1 by 8, A1)
+            note(1 by 8, A1)
+            note(1 by 8, Csharp2)
+        }
     }
 
     track(name = "Vocals", instrument = organs, volume = 0.18f) {
-        note(1 by 4, E4)
-        note(1 by 4, Fsharp4)
-        note(1 by 8, Csharp5)
+        // 8 bars intro (rest)
+        repeat(8) { pause(1 by 1) }
+
+        // Verse 1 melody from MIDI track 3, transposed from G to A major
+        note(1 by 4, Csharp5)
+        note(1 by 4, Csharp5)
+        note(1 by 4, B4)
+        note(5 by 4, A4)
+        note(1 by 2, Csharp5)
+        note(1 by 4, B4)
+        note(9 by 8, A4)
+        note(1 by 8, A4)
+        note(1 by 8, A4)
+        note(1 by 4, Csharp5)
+        note(1 by 4, D5)
+        note(1 by 4, A4)
+        note(1 by 8, A4)
+        note(1 by 8, A4)
+        note(1 by 4, D5)
+        note(1 by 4, Csharp5)
+        note(1 by 4, B4)
         note(1 by 8, B4)
-        note(1 by 8, Gsharp4)
-        note(1 by 8, Fsharp4)
-        note(1 by 8, Gsharp4)
-        note(1 by 8, Fsharp4)
-        note(1 by 8, E4)
-        note(1 by 8, Gsharp4)
-        note(1 by 2, Csharp4)
-
-        note(1 by 4, Gsharp4)
         note(1 by 8, A4)
-        note(1 by 8, Gsharp4)
-        note(1 by 8, E4)
-        note(1 by 8, Csharp4)
-        note(1 by 8, Gsharp4)
-        note(1 by 8, A4)
-        note(1 by 8, Gsharp4)
-        note(1 by 8, E4)
-        note(1 by 8, Csharp4)
-        note(1 by 8, Dsharp4)
-        note(1 by 2, E4)
-
-        note(1 by 4, E4)
-        note(1 by 4, Fsharp4)
-        note(1 by 8, Csharp5)
-        note(1 by 8, B4)
-        note(1 by 8, Gsharp4)
-        note(1 by 8, Fsharp4)
-        note(1 by 8, Gsharp4)
-        note(1 by 8, Fsharp4)
-        note(1 by 8, E4)
-        note(1 by 8, Gsharp4)
-        note(3 by 8, Csharp4)
-        note(1 by 8, Dsharp4)
-        note(1 by 8, E4)
-
-        note(1 by 4, Gsharp4)
-        note(1 by 8, A4)
-        note(1 by 8, Gsharp4)
-        note(1 by 8, E4)
-        note(1 by 8, Csharp4)
-        note(1 by 8, Gsharp4)
-        note(1 by 8, A4)
-        note(1 by 8, Gsharp4)
-        note(1 by 8, E4)
-        note(1 by 8, Csharp4)
-        note(1 by 8, Dsharp4)
-        note(1 by 2, E4)
+        note(1 by 4, Csharp5)
+        note(1 by 4, A4)
+        note(5 by 4, A4)
     }
 }
