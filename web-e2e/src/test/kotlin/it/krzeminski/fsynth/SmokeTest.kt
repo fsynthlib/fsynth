3a6f6482f939de7c6c46a58c9096cbe3a7cad2b8
Piotr Krzeminski <git@krzeminski.it>
2026-05-21 19:07:24 +0200

    Revert changes in e2e tests

--- web-e2e/src/test/kotlin/it/krzeminski/fsynth/SmokeTest.kt
+++ web-e2e/src/test/kotlin/it/krzeminski/fsynth/SmokeTest.kt
@@ -6,7 +6,6 @@ import io.github.bonigarcia.wdm.WebDriverManager
 import java.io.File
 import java.net.InetSocketAddress
 import java.nio.file.Files
-import java.time.Duration
 import org.junit.AfterClass
 import org.junit.Assert.assertTrue
 import org.junit.BeforeClass
@@ -14,7 +13,6 @@ import org.junit.Test
 import org.openqa.selenium.By
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
-import org.openqa.selenium.support.ui.WebDriverWait
 
 class SmokeTest {
     companion object {
@@ -30,13 +28,6 @@ class SmokeTest {
                 createContext("/") { exchange: HttpExchange ->
                     val file = File(webDist, exchange.requestURI.path).normalize()
                     if (file.absolutePath.startsWith(File(webDist).absolutePath) && file.isFile) {
-                        exchange.responseHeaders.add("Content-Type", when {
-                            file.name.endsWith(".js") -> "application/javascript"
-                            file.name.endsWith(".html") -> "text/html"
-                            file.name.endsWith(".css") -> "text/css"
-                            file.name.endsWith(".svg") -> "image/svg+xml"
-                            else -> "application/octet-stream"
-                        })
                         exchange.sendResponseHeaders(200, file.length())
                         Files.copy(file.toPath(), exchange.responseBody)
                     } else {
@@ -47,13 +38,16 @@ class SmokeTest {
                 executor = null
             }
             server.start()
+            println("HTTP server started on port $PORT, serving $webDist")
 
             WebDriverManager.chromedriver().setup()
-            driver = ChromeDriver(ChromeOptions().apply {
+            val options = ChromeOptions().apply {
                 addArguments("--headless")
                 addArguments("--no-sandbox")
                 addArguments("--disable-dev-shm-usage")
-            })
+            }
+            driver = ChromeDriver(options)
+            println("ChromeDriver started")
         }
 
         @AfterClass
@@ -61,6 +55,7 @@ class SmokeTest {
         fun teardown() {
             driver.quit()
             server.stop(0)
+            println("Cleanup done")
         }
     }
 
@@ -68,12 +63,7 @@ class SmokeTest {
     fun titleAndSongListAreDisplayed() {
         driver.get("http://localhost:$PORT/index.html")
 
-        var bodyText = ""
-        WebDriverWait(driver, Duration.ofSeconds(10)).until {
-            bodyText = driver.findElement(By.tagName("body")).text
-            bodyText.contains("fsynth")
-        }
-
+        val bodyText = driver.findElement(By.tagName("body")).text
         assertTrue("Page should contain app title", bodyText.contains("fsynth"))
         assertTrue("Page should contain at least one song name", bodyText.contains("Simple demo song"))
         assertTrue("Page should contain playback customization section", bodyText.contains("Playback customization"))
